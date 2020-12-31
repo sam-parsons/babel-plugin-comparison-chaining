593f9d0837d346e24c8bbc0ab31f0d2add2af653
Sam Parsons <samparsons269@gmail.com>
2020-12-30 17:38:36 -0800

    code refactor, reformat

--- test/index.js
+++ test/index.js
@@ -3,19 +3,23 @@ import path from 'path';
 import { expect } from 'chai';
 import { transformFileSync } from 'babel-core';
 
+const skipArray = ['expression', 'iife'];
+
 describe('Comparison operator chaining', () => {
-    const fixtures = path.join(__dirname, 'fixtures');
-
-    fs.readdirSync(fixtures).forEach(fixture => {
-        it(fixture, () => {
-            const directory = path.join(fixtures, fixture);
-            const actual = transformFileSync(path.join(directory, 'actual.js'), {
-                babelrc: false,
-                plugins: [path.resolve('./src/index.js')]
-            }).code;
-            const expected = fs.readFileSync(path.join(directory, 'expected.js')).toString();
-
-            expect(actual.trim()).to.equals(expected.trim());
-        })
-    });
+  const fixtures = path.join(__dirname, 'fixtures');
+  fs.readdirSync(fixtures).forEach(fixture => {
+    if (!skipArray.includes(fixture)) {
+      it(fixture, () => {
+        const directory = path.join(fixtures, fixture);
+        const actual = transformFileSync(path.join(directory, 'actual.js'), {
+            babelrc: false,
+            plugins: [path.resolve('./src/index.js')]
+        }).code;
+        const expected = fs.readFileSync(path.join(directory, 'expected.js')).toString();
+        expect(actual.trim()).to.equals(expected.trim());
+      })
+    }
+  });
 });
+
+// spies on early return statements in visitor
