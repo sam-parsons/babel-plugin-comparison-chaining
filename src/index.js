35ced7665346c2a722c30c1d6ffeb0c0a6c0275c
Sam Parsons <samparsons269@gmail.com>
2020-12-30 17:33:16 -0800

    lots of reformatting

--- src/index.js
+++ src/index.js
@@ -1,67 +1,73 @@
-function parseExpressionStatement(node, array) {
-  if (node.type === 'BinaryExpression') {
-    array.push(node.right.value);
-    array.push(node.operator);
-    parseExpressionStatement(node.left, array);
-  } else if (node.type === 'NumericLiteral' || node.type === 'StringLiteral') { // need to make this line generalized
-    array.push(node.value);
-  } else return;  
-}
-
 function generateExpressionStatement(t, array) {
   const endOfArr = array.slice(array.length - 3);
 
-  return t.expressionStatement(t.logicalExpression('&&', 
-          generateLogicalExpression(t, array.slice(0, array.length - 2)), 
-          t.binaryExpression(endOfArr[1], 
-              createLiteral(endOfArr[0], t), 
-              createLiteral(endOfArr[2], t))
-        ));
+  return t.expressionStatement(t.logicalExpression(
+    '&&', 
+    generateLogicalExpression(t, array.slice(0, array.length - 2)), 
+    t.binaryExpression(endOfArr[1], 
+      createLiteral(endOfArr[0], t), 
+      createLiteral(endOfArr[2], t))
+  ));
 }
 
 function createLiteral(node, types) {
   switch (typeof node) {
     case "string": return types.stringLiteral(node);
-    case "number": return types.numericLiteral(node)
-    default: return
+    case "number": return types.numericLiteral(node);
+    default: return;
   }
 }
 
+// need to refactor and comment
 function generateLogicalExpression(t, array) {
   const endOfArr = array.slice(array.length - 3);
 
-  if (array.length < 6) return t.logicalExpression('&&', 
-                                    t.binaryExpression(array[array.length - 4], 
-                                      createLiteral(array[array.length - 5], t), 
-                                      createLiteral(array[array.length - 3], t)), 
-                                    t.binaryExpression(array[array.length - 2], 
-                                      createLiteral(array[array.length - 3], t), 
-                                      createLiteral(array[array.length - 1], t)))
+  if (array.length < 6) return t.logicalExpression(
+    '&&', 
+    t.binaryExpression(array[array.length - 4], 
+      createLiteral(array[array.length - 5], t), 
+      createLiteral(array[array.length - 3], t)), 
+    t.binaryExpression(array[array.length - 2], 
+      createLiteral(array[array.length - 3], t), 
+      createLiteral(array[array.length - 1], t)))
 
-  return t.logicalExpression('&&', 
-                  generateLogicalExpression(t, array.slice(0, array.length - 2)), 
-                  t.binaryExpression(endOfArr[1], 
-                    createLiteral(endOfArr[0], t), 
-                    createLiteral(endOfArr[2], t)))
+  return t.logicalExpression(
+    '&&', 
+    generateLogicalExpression(t, array.slice(0, array.length - 2)), 
+    t.binaryExpression(endOfArr[1], 
+    createLiteral(endOfArr[0], t), 
+    createLiteral(endOfArr[2], t))
+  );
 }
 
+function parseExpressionStatement(node, array) {
+  if (node.type === 'BinaryExpression') {
+    array.push(node.right.value);
+    array.push(node.operator);
+    parseExpressionStatement(node.left, array);
+  } else  { // need to make this line generalized
+    array.push(node.value);
+  }
+}
 
-module.exports = ({ types: t }) => ({
+module.exports = ({ types: t }) => (
+  {
     visitor: {
       ExpressionStatement(path) {
         // also check if left side of binary expression is not equal to a binary express
         if (path.node.expression.type !== 'BinaryExpression') return;
+        if (path.node.expression.left.type !== 'BinaryExpression') return;
 
-        const array = [];
-        
-        if (path.node.expression.type === 'BinaryExpression') {
-          array.push(path.node.expression.right.value);
-          array.push(path.node.expression.operator);
-          parseExpressionStatement(path.node.expression.left, array);
-        }  
+        //
+        const array = [path.node.expression.right.value, path.node.expression.operator];
 
+        //
+        parseExpressionStatement(path.node.expression.left, array); 
+
+        //
         array.reverse();
 
+        //
         const newAST = generateExpressionStatement(t, array);
 
         path.replaceWith(newAST); 
@@ -69,4 +75,5 @@ module.exports = ({ types: t }) => ({
         return;
       }
     }
-});
+  }
+);
